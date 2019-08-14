4d1f9f8e052c3610e3f9f79934e8ffdfab2b6604
GTimothee <ava_punksmash@protonmail.com>
2019-08-14 17:23:46 -0400

    travis working, testing all tests with pytest

--- scripts/install_and_run.sh
+++ scripts/install_and_run.sh
@@ -6,4 +6,4 @@ export PYTHONPATH="$PWD:$PWD/src:$PWD/dask:$PYTHONPATH:"
 echo "PYTHONPATH set to $PYTHONPATH"
 export DATA_PATH='data'
 export OUTPUT_DIR='output_imgs'
-pipenv run pytest tests/test_main.py
+pipenv run pytest tests
