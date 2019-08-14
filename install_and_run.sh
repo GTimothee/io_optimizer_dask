01bdbd9e276060d17d7bf8970fa0229de0ce83ab
GTimothee <ava_punksmash@protonmail.com>
2019-08-14 16:25:40 -0400

    try adding src

--- install_and_run.sh
+++ install_and_run.sh
@@ -1,7 +1,7 @@
 mkdir data
 git clone https://github.com/GTimothee/dask_utils_perso.git
 git clone https://github.com/GTimothee/dask.git
-export PYTHONPATH="$PWD:$PWD/dask:$PYTHONPATH:"
+export PYTHONPATH="$PWD:$PWD/src:$PWD/dask:$PYTHONPATH:"
 echo "PYTHONPATH set to $PYTHONPATH"
 export DATA_PATH='data'
 export OUTPUT_DIR='output_imgs'
