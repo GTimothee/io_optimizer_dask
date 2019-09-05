9c3207f135c7a71faa9102a805eadca1c1870b99
GTimothee <ava_punksmash@protonmail.com>
2019-09-05 17:16:36 -0400

    added out folder creation

--- benchmarks/benchmarks/speed_tests.py
+++ benchmarks/benchmarks/speed_tests.py
@@ -70,7 +70,6 @@ def run(is_scheduler, optimize, arr, buffer_size):
     t = time.time()
     res = arr.compute()
     t = time.time() - t
-    visualize([prof, rprof, cacheprof])
     unregister_profilers()
     
     return res, t
@@ -96,25 +95,40 @@ def _sum(non_opti, opti, buffer_size, shapes_to_test, chunks_to_test):
 
         for is_scheduler in [False, True]:
             scheduler_status = 'scheduler_on' if is_scheduler else 'scheduler_off'
+            sched_out_path = os.path.join(output_dir, scheduler_status)
+            os.mkdir(sched_out_path, '0755') if not os.exists(sched_out_path)
 
-            for nb_chunks in chunks_to_test[scheduler_status]:
-                for chunk_shape in shapes_to_test: # tests_utils.chunk_shapes:  
-                    
+            for chunk_shape in shapes_to_test: 
+                chunk_path = os.path.join(sched_out_path, chunk_shape)
+                os.mkdir(chunk_path, '0755') if not os.exists(sched_out_path)
+
+                for nb_chunks in chunks_to_test[chunk_shape][scheduler_status]:
+                    nb_chunk_path = os.path.join(chunk_path, str(nb_chunks) + '_chunks')
+                    os.mkdir(nb_chunk_path, '0755') if not os.exists(sched_out_path)
+
                     if non_opti:
                         arr = get_test_arr(case='sum', nb_arr=nb_chunks)
                         res_dask, t = run(is_scheduler, False, nb_chunks, arr, buffer_size)
+                        
+                        out_file_path = os.path.join(nb_chunk_path, 'non_opti')
+                        visualize([prof, rprof, cacheprof], out_file_path)
+
                         writer.writerow([False, is_scheduler, chunk_shape, nb_chunks, buffer_size, t])
 
                     if opti:
                         arr = get_test_arr(case='sum', nb_arr=nb_chunks)
                         res_opti, t = run(is_scheduler, True, nb_chunks, arr, buffer_size)
+
+                        out_file_path = os.path.join(nb_chunk_path, 'non_opti')
+                        visualize([prof, rprof, cacheprof], out_file_path)
+
                         writer.writerow([True, is_scheduler, chunk_shape, nb_chunks, buffer_size, t])
 
                     if opti and non_opti:
                         assert np.array_equal(res_dask, res_opti)
 
 
-def _store():
+def _store(non_opti, opti):
     def get_datasets(file_name, a1, a2):
         file_path = os.path.join(file_name)
         if os.path.isfile(file_path):
@@ -134,7 +148,7 @@ def _store():
         _, t = run(None, False, arr, buffer_size)
         f.close()
     
-    
+
     if opti:
         for is_scheduler in [False, True]:
             arr = get_test_arr()
@@ -147,12 +161,26 @@ def _store():
 
 
 def benchmark():
-    shapes_to_test = ["blocks_dask_interpol", "slabs_dask_interpol"]
+    
     chunks_to_test = {
-        True: [105, 210],
-        False: [105],
+        'slabs_dask_interpol': {
+            True: [105, 210],
+            False: [105]},
+        'slabs_previous_exp': {
+            True: [105, 210],
+            False: [105]},
+        'blocks_dask_interpol':{
+            True: [105, 210],
+            False: [105]}, 
+        'blocks_previous_exp': {
+            True: [105, 210],
+            False: [105]}
     }
-    sum_scheduler_opti(True, False, 5 * ONE_GIG, shapes_to_test, chunks_to_test)   
+
+    non_opti, opti = (True, False)
+    buffer_size = 5 * ONE_GIG
+    shapes_to_test = ["blocks_dask_interpol", "slabs_dask_interpol"]
+    sum_scheduler_opti(non_opti, opti, buffer_size, shapes_to_test, chunks_to_test)   
 
 benchmark()
 # _store()
