7e4c5b9f38a99fe9539cab963f571c2257c0d1f6
GTimothee <ava_punksmash@protonmail.com>
2019-09-09 18:34:11 -0400

    refactored speed_tests by creating little functions, adding a configuration object and shuffling tests

--- benchmarks/benchmarks/speed_tests.py
+++ benchmarks/benchmarks/speed_tests.py
@@ -5,6 +5,7 @@ import time
 import numpy as np
 import csv
 import h5py
+from random import shuffle
 
 import dask
 import dask.array as da
@@ -57,6 +58,31 @@ def run(is_scheduler, optimize, arr, buffer_size):
     return res, t
 
 
+class Test_config():
+    """ Contains the configuration for a test.
+    """
+    def __init__(self, opti, scheduler_opti, out_file_path, buffer_size):
+        self.test_case = None
+        self.opti = opti 
+        self.scheduler_opti = scheduler_opti
+        self.out_file_path = out_file_path
+        self.buffer_size = buffer_size
+
+        # default to not recreate file
+        self.chunk_shape = None 
+        self.shape = None
+        self.overwrite = None
+
+    def sum_case(self, nb_chunks):
+        self.test_case = 'sum'
+        self.nb_chunks = nb_chunks
+
+    def create_or_overwrite(self, chunk_shape, shape, overwrite):
+        self.chunk_shape = chunk_shape
+        self.shape = shape
+        self.overwrite = overwrite
+
+
 def _sum(non_opti, opti, buffer_size, shapes_to_test, chunks_to_test):
     """ Test if the sum of n blocks yields the good result.
 
@@ -74,83 +100,80 @@ def _sum(non_opti, opti, buffer_size, shapes_to_test, chunks_to_test):
             os.mkdir(path) 
         return path
 
-    output_dir = os.environ.get('OUTPUT_BENCHMARK_DIR')
-    data_path = get_test_array()
+    def run_test(config):
+        """ Run a test given a specific configuration.
+        """
+        if config.test_case != 'sum':
+            raise ValueError("Configuration not prepared for sum test case.")
+
+        # get array
+        file_path = os.path.join(os.getenv('DATA_PATH'), 'sample_array.hdf5')
+        arr = get_test_arr(file_path, 
+                           chunk_shape=config.chunk_shape, 
+                           shape=config.shape, 
+                           test_case='sum', 
+                           nb_chunks=config.nb_chunks, 
+                           overwrite=config.overwrite)
+
+        # run test
+        with Profiler() as prof, ResourceProfiler() as rprof, CacheProfiler(metric=nbytes) as cprof:
+            res_dask, t = run(config.scheduler_opti, 
+                              config.opti, 
+                              arr, 
+                              config.buffer_size)
+
+        # write output
+        out_file_path = os.path.join(out_path, 'non_opti.html')
+        visualize([prof, rprof, cprof], config.out_file_path)
+        writer.writerow([config.opti, 
+                         config.scheduler_opti, 
+                         config.chunk_shape, 
+                         config.nb_chunks, 
+                         config.buffer_size, t])
+
+    # create the tests to be run
+    # create the output directories
+    output_dir = os.environ.get('OUTPUT_BENCHMARK_DIR')  
+    tests = list()
+    for is_scheduler in [True, False]:
+        scheduler_status = 'scheduler_on' if is_scheduler else 'scheduler_off'
+        sched_out_path = add_dir(output_dir, scheduler_status)
+
+        for chunk_shape in shapes_to_test: 
+            chunk_path = add_dir(sched_out_path, chunk_shape)
+            
+            for nb_chunks in chunks_to_test[chunk_shape][scheduler_status]:
+                out_path = add_dir(chunk_path, str(nb_chunks) + '_chunks')
+
+                if non_opti:
+                    new_config = Test_config(False, False, out_file_path, buffer_size)
+                    new_config.sum_case(nb_chunks)
+                    tests.append(new_config)
+
+                if opti:
+                    new_config = Test_config(True, scheduler_opti, out_file_path, buffer_size)
+                    new_config.sum_case(nb_chunks)
+                    tests.append(new_config)
+
+    # run the tests
     with open(os.path.join(output_dir, 'sum_speeds.csv'), mode='a+') as csv_out:
         writer = csv.writer(csv_out, delimiter=',')
         writer.writerow(['optimized', 'is_scheduler', 'chunk_shape', 
                          'nb_chunks_to_sum', 'buffer_size', 'processing_time'])
-
-        for is_scheduler in [True]:
-            scheduler_status = 'scheduler_on' if is_scheduler else 'scheduler_off'
-            sched_out_path = add_dir(output_dir, scheduler_status)
-
-            for chunk_shape in shapes_to_test: 
-                chunk_path = add_dir(sched_out_path, chunk_shape)
-                
-                for nb_chunks in chunks_to_test[chunk_shape][scheduler_status]:
-                    out_path = add_dir(chunk_path, str(nb_chunks) + '_chunks')
-
-                    if non_opti:
-                        arr = get_test_arr(case='sum', nb_arr=nb_chunks)
-                        out_file_path = os.path.join(out_path, 'non_opti.html')
-                        with Profiler() as prof, ResourceProfiler() as rprof, CacheProfiler(metric=nbytes) as cprof:
-                            res_dask, t = run(is_scheduler, False, arr, buffer_size)
-                        visualize([prof, rprof, cprof], out_file_path)
-                        writer.writerow([False, is_scheduler, chunk_shape, nb_chunks, buffer_size, t])
-
-                    if opti:
-                        arr = get_test_arr(case='sum', nb_arr=nb_chunks)
-                        out_file_path = os.path.join(out_path, 'opti.html')
-                        with Profiler() as prof, ResourceProfiler() as rprof, CacheProfiler(metric=nbytes) as cprof:
-                            res_opti, t = run(is_scheduler, True, arr, buffer_size)
-                        visualize([prof, rprof, cprof], out_file_path)
-                        writer.writerow([True, is_scheduler, chunk_shape, nb_chunks, buffer_size, t])
-
-                    if opti and non_opti:
-                        assert np.array_equal(res_dask, res_opti)
+        
+        shuffle(tests)
+        for config in tests:
+            run_test(config)
 
 
-def _store(non_opti, opti):
-    def get_datasets(file_name, a1, a2):
-        file_path = os.path.join(file_name)
-        if os.path.isfile(file_path):
-            os.remove(file_path)
-        f = h5py.File(file_path, 'w')
-        dset1 = f.create_dataset('/data1', shape=a1.shape)
-        dset2 = f.create_dataset('/data2', shape=a2.shape)
-        return f, dset1, dset2
-
-    
-    if non_opti:
-        arr = get_test_arr()
-        a1 = arr[:440,:,:]
-        a2 = arr[:440,:,:]
-        f, dset1, dset2 = get_datasets("file1.hdf5", a1, a2)
-        arr = da.store([a1, a2], [dset1, dset2], compute=False)
-        _, t = run(None, False, arr, buffer_size)
-        f.close()
-    
-
-    if opti:
-        for is_scheduler in [False, True]:
-            arr = get_test_arr()
-            a1 = arr[:440,:,:]
-            a2 = arr[:440,:,:]
-            f, dset1, dset2 = get_datasets("file1.hdf5", a1, a2)
-            arr = da.store([a1, a2], [dset1, dset2], compute=False)
-            _, t = run(is_scheduler, True, arr, buffer_size)
-            f.close()
-
-
-def benchmark():
+if __name__ == '__main__':
     chunks_to_test = {
         'slabs_dask_interpol': {
             'scheduler_on': [105, 210],
             'scheduler_off': [105]},
 
         'slabs_previous_exp': {
-            'scheduler_on': [105, 210],
+            'scheduler_on': [105],
             'scheduler_off': [105]},
 
         'blocks_dask_interpol':{
@@ -158,15 +181,11 @@ def benchmark():
             'scheduler_off': [105]}, 
 
         'blocks_previous_exp': {
-            'scheduler_on': [105, 210],
+            'scheduler_on': [105],
             'scheduler_off': [105]}
     }
 
     non_opti, opti = (True, True)
     buffer_size = 5 * ONE_GIG
-    shapes_to_test = ["blocks_dask_interpol", "slabs_dask_interpol"] #, "slabs_dask_interpol"]
+    shapes_to_test = ["blocks_dask_interpol", "slabs_dask_interpol"] 
     _sum(non_opti, opti, buffer_size, shapes_to_test, chunks_to_test)   
-
-if __name__ == '__main__':
-    benchmark()
-# _store()
