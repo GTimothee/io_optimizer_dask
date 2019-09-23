33281d7518bb9d019f580853157d76d307705909
GTimothee <ava_punksmash@protonmail.com>
2019-09-23 17:29:44 -0400

    updated pythonpath

--- scripts/install_and_run.sh
+++ scripts/install_and_run.sh
@@ -2,7 +2,7 @@ mkdir data
 mkdir output_imgs
 git clone https://github.com/GTimothee/dask_utils_perso.git
 git clone https://github.com/GTimothee/dask.git
-export PYTHONPATH="$PWD:$PWD/src:$PWD/dask:$PYTHONPATH:"
+export PYTHONPATH="$PWD:$PWD/src:$PWD/dask:$PYTHONPATH:$PWD/dask_utils_perso:$PWD/dask_utils_perso/dask_utils_perso"
 echo "PYTHONPATH set to $PYTHONPATH"
 export DATA_PATH='data'
 export OUTPUT_DIR='output_imgs'
